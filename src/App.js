a76389a92e9ac7433e98c5b7c2be64c0b8830f54
harutas <96802323+harutas@users.noreply.github.com>
2022-06-01 20:22:03 +0900

    Created Detail view

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
 import './App.css';
+import DetailPage from './components/DetailPage';
+import Header from './components/Header';
 
 function App() {
   return (
     <div className="App">
+      <Header />
+      <div className='conttainer'>
+        <div className='d-flex flex-column align-items-center'>
+
+
+          <DetailPage />
+
+        </div>
+
+      </div>
     </div>
   );
 }
---
+++ src/components/IconButtons.jsx
@@ -0,0 +1,29 @@
+import Stack from '@mui/material/Stack';
+import IconButton from '@mui/material/IconButton';
+import ChatBubbleOutlineIcon from '@mui/icons-material/ChatBubbleOutline';
+import LoopIcon from '@mui/icons-material/Loop';
+import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
+import FileUploadIcon from '@mui/icons-material/FileUpload';
+
+const IconButtons = () => {
+    return (
+        <>
+            <Stack direction="row" justifyContent="space-around" sx={{my: 0.5}}>
+                <IconButton aria-label="reply" size="small">
+                    <ChatBubbleOutlineIcon fontSize="small" />
+                </IconButton>
+                <IconButton aria-label="retweet" size="small">
+                    <LoopIcon fontSize="small" />
+                </IconButton>
+                <IconButton aria-label="favorite" size="small">
+                    <FavoriteBorderIcon fontSize="small" />
+                </IconButton>
+                <IconButton aria-label="file-upload" size="small">
+                    <FileUploadIcon fontSize="small" />
+                </IconButton>
+            </Stack>
+        </>
+    )
+}
+
+export default IconButtons
